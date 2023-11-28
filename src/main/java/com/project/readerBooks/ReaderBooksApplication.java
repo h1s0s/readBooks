932d9c4579eba3c7cac280504ed084d6449c0191
한상선 <hs94122@naver.com>
2023-11-28 14:24:06 +0900

    Initial commit

---
+++ src/main/java/com/project/readerBooks/ReaderBooksApplication.java
@@ -0,0 +1,13 @@
+package com.project.readerBooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaderBooksApplication.class, args);
+	}
+
+}
